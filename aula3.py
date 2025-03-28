a8fc806bff36dc8ab9e4bef7ace9cf7a1b3970e1
TlFabri <nysevnine@gmail.com>
2025-03-28 13:34:14 -0300

    Aspas simples/Duplas

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+    Python = Linguagem de Programação
+    Tipo de tipagem = Dinâmica / Forte
+    str -> string -> texto
+    Strings são textos que estão dentro de aspas
+"""
+# Aspas simples
+print('Thiago')
+
+# Aspas duplas
+print("Fabri")
+
+#Escape 
+print('Thiago \"Fabri\"')
+
+#r
+print(r'Thiago \"Fabri\"')
+
+# Melhor jeito
+print('"Thiago Fabri"')
